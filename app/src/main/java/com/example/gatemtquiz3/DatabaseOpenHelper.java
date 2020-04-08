42bbc62af4c014843fa4d39acfbf514fcb39829d
dinesh <sirimalladinesh@gmail.com>
2020-04-08 20:20:01 +0530

    third commit -- final -- version 0.0

--- app/src/main/java/com/example/gatemtquiz3/DatabaseOpenHelper.java
+++ app/src/main/java/com/example/gatemtquiz3/DatabaseOpenHelper.java
@@ -1,11 +1,12 @@
 package com.example.gatemtquiz3;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "GateMTQuiz2.db";
+    private static final String DATABASE_NAME = "GateMTQuiz4.db";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseOpenHelper(Context context) {
